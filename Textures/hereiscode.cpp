e53a42e9723d0116234b737a9bad841fca33bb66
Dustin Freeman <dustin.freeman@gmail.com>
2017-12-07 11:48:17 -0500

    Added fake code

---
+++ Textures/hereiscode.cpp
@@ -0,0 +1,19 @@
+
+/* Look it's a code file
+
+GOTO 10
+PRINT 20
+GOTO 30
+GOTO 10
+PRINT 20
+GOTO 30
+GOTO 10
+PRINT 20
+GOTO 30
+GOTO 10
+PRINT 20
+GOTO 30
+
+*/
+//end basic code file
+//This is 100% real code I promise
